1927b325fc36efa16771c56fcb22c526818be496
Rajnish2105 <166371359+Rajnish2105@users.noreply.github.com>
2024-11-26 14:02:19 +0530

    final v1

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function App() {
         {/* Show MJPEG stream when detection is started */}
         {isDetecting && (
           <img
-            src="https://4a14-122-15-88-227.ngrok-free.app/video_feed"
+            src="https://f824-1-187-233-7.ngrok-free.app/video_feed"
             alt="MJPEG Stream"
             onError={handleError}
           />
